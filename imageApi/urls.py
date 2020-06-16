4198a82081d34ac009707ff2e0ee8bf78c38d8ed
Karan Modh <karanmodh71@gmail.com>
2020-06-16 14:02:51 +0530

    Done with clening up files and formating the site

--- imageApi/urls.py
+++ imageApi/urls.py
@@ -8,7 +8,7 @@ from core import urls as coreUrls
 urlpatterns = [
     # path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
-    path('upload/', include(coreUrls))
+    path('', include(coreUrls))
 ]
 
 if settings.DEBUG:
